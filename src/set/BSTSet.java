468607ebdb0f40f4806ceda58cbcfdef96c328d5
kang <1037650891@qq.com>
2021-11-24 23:50:40 +0800

    1、添加 二分搜索树的 treeSet

---
+++ src/set/BSTSet.java
@@ -0,0 +1,40 @@
+package set;
+
+import bst.BST;
+
+public class BSTSet<E extends Comparable<E>> implements Set<E>{
+
+    private BST<E> bst;
+
+    public BSTSet(){
+        bst = new BST<>();
+    }
+
+    @Override
+    public void add(E e) {
+        bst.add(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        bst.remove(e);
+    }
+
+    @Override
+    public int size() {
+        return bst.size();
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return bst.contains(e);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return bst.isEmpty();
+    }
+
+
+
+}
---
+++ src/set/Set.java
@@ -0,0 +1,15 @@
+package set;
+
+public interface Set<E> {
+
+    void add(E e);
+
+    void remove(E e);
+
+    int size();
+
+    boolean contains(E e);
+
+    boolean isEmpty();
+
+}
